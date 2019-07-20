fdb1f05a79231feec084fbb4952951f0220f8975
MattHughson! <matt@koodomasson.com>
2019-07-20 13:30:17 +0000

    frontend created

--- backend/db/knex_migrations/20190718191052_create_users.js
+++ backend/db/knex_migrations/20190718191052_create_users.js
@@ -1,5 +1,13 @@
 
 exports.up = function(knex) {
+  return knex.schema.createTable('users', t =>{
+    t.increments('id').unsigned().primary();
+    t.string('email')
+    t.string('password')
+    t.string('phone_number')
+    t.string('connections')
+    
+  })
   
 };
 
