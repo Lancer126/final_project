5cf67a3915bce1204d87769ca9fde3010dceb1c6
Harrison2301 <harrisonwinters2301@gmail.com>
2019-07-21 20:29:52 +0000

    implement google map api on maps

--- client/src/components/Map.jsx
+++ client/src/components/Map.jsx
@@ -1,13 +1,24 @@
 import React, { Component } from 'react';
-
-class Map extends Component {
+import {Map, InfoWindow, Marker, GoogleApiWrapper} from 'google-maps-react';
+ 
+export class MapContainer extends Component {
   render() {
     return (
-        <div>
-          <h3>Map</h3>
-        </div>
+      <Map google={this.props.google} zoom={14}>
+ 
+        <Marker onClick={this.onMarkerClick}
+                name={'Current location'} />
+ 
+        <InfoWindow onClose={this.onInfoWindowClose}>
+            <div>
+              {/* <h1>{this.state.selectedPlace.name}</h1> */}
+            </div>
+        </InfoWindow>
+      </Map>
     );
   }
 }
-
-export default Map;
+ 
+export default GoogleApiWrapper({
+  apiKey: ("AIzaSyBmOjKnCb27NSuwCaY98GW-m0A9Rk9x9eE")
+})(MapContainer)
