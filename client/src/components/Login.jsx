c590083f6fe24dc15aa0dcc5e831a55b0198b1f9
Lancer126 <rthcontact@gmail.com>
2019-07-23 15:00:58 +0000

    Implemented Facebook login

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -6,6 +6,8 @@ class Login extends Component {
         <div>
           <button>Login</button>
           <button>Register</button>
+          <br/>
+          <a href="/auth/facebook">Login with Facebook</a>
 
         </div>
     );
