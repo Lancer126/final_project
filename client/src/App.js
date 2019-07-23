06a1b4b33964ffa456d4feda9194fb836d40ff6c
Harrison2301 <harrisonwinters2301@gmail.com>
2019-07-23 20:53:12 +0000

    clean code

--- client/src/App.js
+++ client/src/App.js
@@ -11,47 +11,47 @@ import './App.css';
 
 class App extends Component {
 
-constructor(props){
-  super(props);
-  this.state = {
-    events: [
-      //{name: 'catalina wine mixer'},
-      //{name: 'osheaga'}
-    ]
+  constructor(props) {
+    super(props);
+    this.state = {
+      events: [
+        //{name: 'catalina wine mixer'},
+        //{name: 'osheaga'}
+      ]
+    }
   }
-}
 
- componentDidMount() {
-   fetch('/event').then(data => data.json()).then(response => this.setState({events: response}));
+  componentDidMount() {
+    fetch('/event').then(data => data.json()).then(response => this.setState({ events: response }));
 
-   console.log(this.state);
- }
+    console.log(this.state);
+  }
 
   render() {
     return (
-    <Router>
+      <Router>
         <div>
-        <h1>Eventure</h1>
+          <h1>Eventure</h1>
           <nav className="navbar navbar-expand-lg navbar-light bg-light">
-     
-          <button><Link to={'/'} className="nav-link"> Login </Link></button>
-          <button><Link to={'/map'} className="nav-link"> Map </Link></button>
-          <button><Link to={'/discover'} className="nav-link"> Discover </Link></button>
-          <button><Link to={'/search'} className="nav-link"> Search </Link></button>
-          <button><Link to={'/tickets'} className="nav-link"> Tickets </Link></button>
-          <button><Link to={'/aid'} className="nav-link"> Aid </Link></button>
-          <button><Link to={'/profile'} className="nav-link"> Profile </Link></button>
-        
+
+            <button><Link to={'/'} className="nav-link"> Login </Link></button>
+            <button><Link to={'/map'} className="nav-link"> Map </Link></button>
+            <button><Link to={'/discover'} className="nav-link"> Discover </Link></button>
+            <button><Link to={'/search'} className="nav-link"> Search </Link></button>
+            <button><Link to={'/tickets'} className="nav-link"> Tickets </Link></button>
+            <button><Link to={'/aid'} className="nav-link"> Aid </Link></button>
+            <button><Link to={'/profile'} className="nav-link"> Profile </Link></button>
+
           </nav>
           <hr />
           <Switch>
-              <Route exact path='/' component={Login} />
-              <Route path='/map' render={() => <Map events={this.state.events} /> } />
-              <Route path='/discover' render={() => <Discover events={this.state.events} /> } />
-              <Route path='/search'  component={Search}/>
-              <Route path='/tickets' component={Tickets}/>
-              <Route path='/aid' component={Aid}/>
-              <Route path='/profile' component={Profile}/>
+            <Route exact path='/' component={Login} />
+            <Route path='/map' render={() => <Map events={this.state.events} />} />
+            <Route path='/discover' render={() => <Discover events={this.state.events} />} />
+            <Route path='/search' component={Search} />
+            <Route path='/tickets' component={Tickets} />
+            <Route path='/aid' component={Aid} />
+            <Route path='/profile' component={Profile} />
 
           </Switch>
         </div>
