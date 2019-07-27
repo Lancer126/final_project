b8d8e67b91e26252d674a6e390907be81a6d4c66
MattHughson! <matt@koodomasson.com>
2019-07-27 18:24:08 +0000

    adding migration for facebook authentication

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,7 @@ const http = require('http');
 const express = require('express');
 const MessagingResponse = require('twilio').twiml.MessagingResponse;
 
-const app = express();
+
 
 app.post('/sms', (req, res) => {
   const twiml = new MessagingResponse();
